4979d9c1ac9549c1a3a2c6f1cb532700c8bad31f
Sophie <sophielennon-x@live.co.uk>
2017-09-28 15:07:26 +0000

    Adding more products to the database

---
+++ SQL Script/AddingMoreProducts.sql
@@ -0,0 +1,16 @@
+use QaCinema
+
+insert into Products (Name, Size, Type, Description, Price, Colour, Image, PromotionalProduct)
+values ('Superman Soft Toy ', 'Medium', 'Toy', 'Plush toy perfect for any occasion', 12.99, 'Blue', 'C:\Users\Administrator\Desktop\superman.jpg', 1)
+
+insert into Products (Name, Size, Type, Description, Price, Colour, Image, PromotionalProduct)
+values ('Batman Soft Toy ', 'Medium', 'Toy', 'Plush toy perfect for any occasion', 12.99, 'Black', 'C:\Users\Administrator\Desktop\batman.jpg', 0)
+
+insert into Products (Name, Size, Type, Description, Price, Colour, Image, PromotionalProduct)
+values ('Wolverine Cooking Apron', 'Large', 'Apron', 'Time to live that dream of being wolverine', 25.99, 'Black', 'C:\Users\Administrator\Desktop\wolverine.jpg', 1)
+
+insert into Products (Name, Size, Type, Description, Price, Colour, Image, PromotionalProduct)
+values ('Thor Wallet', 'Small', 'Wallet', 'Thor Ragnarok Mens Bi-Fold Wallet', 24.99, 'Grey', 'C:\Users\Administrator\Desktop\thor.jpg', 1)
+
+select *
+from products
